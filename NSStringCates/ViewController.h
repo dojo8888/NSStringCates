bdeffdd7a79f1b85a9a797e50785914162d51f9d
dojo8888 <dojo8888@gmail.com>
2014-01-20 17:06:28 +0900

    Initial Commit

---
+++ NSStringCates/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  NSStringCates
+//
+//  Created by KangNamgyu on 2014. 1. 20..
+//  Copyright (c) 2014년 KangNamgyu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
